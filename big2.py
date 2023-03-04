6132be6957e87843c53417a1fa1b01ab43f81da6
Conway <yawnoc@pm.me>
2023-03-04 23:42:03 +0800

    Implement read scores text

--- big2.py
+++ big2.py
@@ -10,10 +10,10 @@ Licensed under MIT No Attribution (MIT-0), see LICENSE.
 """
 
 import argparse
-
+import os
+import sys
 
 __version__ = '0.0.0'
-
 
 DESCRIPTION = 'Score some games of Big Two (鋤大弟).'
 
@@ -34,8 +34,32 @@ def parse_command_line_arguments():
     return argument_parser.parse_args()
 
 
+def read_scores_text(scores_file_name):
+    if os.path.isdir(scores_file_name):
+        print(
+            f'Error: `{scores_file_name}` is a directory, not a file',
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    try:
+        with open(scores_file_name, 'r', encoding='utf-8') as scores_file:
+            scores_text = scores_file.read()
+    except FileNotFoundError:
+        print(
+            f'Error: file `{scores_file_name}` not found',
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    return scores_text
+
+
 def main():
     parsed_arguments = parse_command_line_arguments()
+    scores_file_name = parsed_arguments.scores_file_name
+
+    scores_text = read_scores_text(scores_file_name)
 
 
 if __name__ == '__main__':
