285050a632e97b7bb323212361d20a08fb3ff565
Conway <yawnoc@pm.me>
2023-03-04 23:37:20 +0800

    Implement CLI parsing

--- big2.py
+++ big2.py
@@ -9,9 +9,33 @@ Copyright 2023 Conway
 Licensed under MIT No Attribution (MIT-0), see LICENSE.
 """
 
+import argparse
+
+
+__version__ = '0.0.0'
+
+
+DESCRIPTION = 'Score some games of Big Two (鋤大弟).'
+
+
+def parse_command_line_arguments():
+    argument_parser = argparse.ArgumentParser(description=DESCRIPTION)
+    argument_parser.add_argument(
+        '-v', '--version',
+        action='version',
+        version=f'{argument_parser.prog} version {__version__}',
+    )
+    argument_parser.add_argument(
+        'scores_file_name',
+        help='name of scores file',
+        metavar='scores.txt',
+    )
+
+    return argument_parser.parse_args()
+
 
 def main():
-    pass
+    parsed_arguments = parse_command_line_arguments()
 
 
 if __name__ == '__main__':
