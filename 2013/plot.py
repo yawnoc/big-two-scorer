e2a6498dae2984d1b539ac47b1ed8b682629b32d
Conway <yawnoc@pm.me>
2023-03-05 13:19:52 +0800

    Implement plot.py make_plot

--- 2013/plot.py
+++ 2013/plot.py
@@ -12,6 +12,8 @@ Licensed under MIT No Attribution (MIT-0), see LICENSE.
 
 import csv
 
+import matplotlib.pyplot as plt
+
 
 def read_scores(file_name):
     with open(file_name, 'r', encoding='utf-8', newline='') as file:
@@ -21,7 +23,37 @@ def read_scores(file_name):
 
 
 def make_plot(rows, x_variable, y_variable):
-    pass
+    regular_rows = [r for r in rows if r['is_regular'] == 'True']
+
+    x_values = [float(r[x_variable]) for r in regular_rows]
+    y_values = [float(r[y_variable]) for r in regular_rows]
+    names = [r['name'] for r in regular_rows]
+    data_count = len(regular_rows)
+
+    figure, axes = plt.subplots()
+    axes.scatter(
+        x_values,
+        y_values,
+        s=100,
+        facecolors='none',
+        edgecolors=(data_count - 1) * ['blue'] + ['red'],
+    )
+    for x, y, name in zip(x_values, y_values, names):
+        axes.annotate(
+            name,
+            (x, y),
+            xytext=(7, 2),
+            textcoords='offset points',
+        )
+
+    axes.set(
+        title=f'{x_variable} vs {y_variable}',
+        xlabel=x_variable,
+        ylabel=y_variable,
+    )
+    axes.grid(True)
+
+    plt.savefig(f'{x_variable}-vs-{y_variable}.svg')
 
 
 def main():
